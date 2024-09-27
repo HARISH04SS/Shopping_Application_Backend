06222302400358d1b804bc6536e5ab4d2b199c4e
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-28 00:37:30 +0530

    user able to login

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userSchema')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../utils/config');
 const userController = {
     register: async (req,res) => {
         try{
@@ -14,7 +16,24 @@ const userController = {
             console.log('User saved successfully');
             res.status(201).json({message:"User created Succesfully"});
         }catch(error){
-            res.status(500).json({message:error.message})
+            res.status(500).json({message:error.message});
+        }
+    },
+    login: async (req,res)=>{
+        try{
+            const {phonenumber, password} = req.body;
+            const user = await User.findOne({phonenumber});
+            if(!user){
+                return res.status(400).json({message:"User not registered"});
+            }
+            const validPassword = await bcrypt.compare(password,user.password);
+            if (!validPassword) {
+                return res.status(400).json({ message: 'Invalid password' });
+            }
+            const token = jwt.sign({id: user._id},jwtSecret)
+            res.json({ message: 'Login successful' ,token});
+        }catch(error){
+            res.status(500).json({message:error.message});
         }
     }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const userRoute = express.Router();
-userRoute.post('/register',userController.register)
-
+userRoute.post('/register',userController.register);
+userRoute.post('/login',userController.login);
 module.exports = userRoute;
